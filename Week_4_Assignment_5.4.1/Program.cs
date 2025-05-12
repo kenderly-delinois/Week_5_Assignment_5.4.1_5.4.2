1b9e75504d9872c0cb03fd034b81011d0f6f8d2e
kende <kende@KENNY>
2025-05-12 07:23:52 -0500

    Add project files.

---
+++ Week_4_Assignment_5.4.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Week_4_Assignment_5._4._1
+{
+    internal class Program
+    {
+//        Write a program in C# Sharp to display the individual digits of a given number using recursion.
+//        Test Data :
+//        Input any number : 1234
+//        Expected Output :
+//T       he digits in the number 1234 are : 1 2 3 4
+       static void DisplayIndividual(int n)
+        {
+            if(n < 10)
+            {
+                Console.WriteLine(n + " ");
+                return;   
+            }
+            DisplayIndividual(n / 10);
+            Console.WriteLine(((n % 10) + " "));
+        }
+        static void Main(string[] args)
+        {
+            int num = 1234;
+            Console.Write(num);
+        }
+    }
+}
---
+++ Week_5_Assignment_5.4.2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq.Expressions;
+using System.Xml.Linq;
+
+namespace Week_5_Assignment_5._4._2
+{
+    internal class Program
+    {
+        //Write a C# Sharp program to find the sum of the right diagonals of a matrix.
+        //Test Data :
+        //Input the size of the square matrix : 2
+        //Input elements in the first matrix :
+        //element - [0],[0] : 1
+        //element - [0],[1] : 2
+        //element - [1],[0] : 3
+        //element - [1],[1] : 4
+        //Expected Output :
+        //The matrix is :
+        //1 2
+        //3 4
+        //Addition of the right Diagonal elements is :5
+
+
+        static void Main(string[] args)
+        {
+            int size = 2;
+            int[,] matrix = new int[size, size];
+
+            // Input elements of the matrix
+            Console.WriteLine("Input elements in the first matrix:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write($"element - [{i}],[{j}] : ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            // Display the matrix
+            Console.WriteLine("The matrix is:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Calculate right diagonal sum
+            int rightDiagonalSum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                rightDiagonalSum += matrix[i, size - i - 1];
+            }
+
+            Console.WriteLine("Addition of the right Diagonal elements is: " + rightDiagonalSum);
+        }
+    }
+    }
+
